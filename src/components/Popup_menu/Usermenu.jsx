6f06fa1b3f6be32efd2faffb478da14e487abc60
akshayDeq <akanojiya@deqode.com>
2022-03-15 11:56:40 +0530

    Create : User menu popup component

---
+++ src/components/Popup_menu/Usermenu.jsx
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from "react";
+import "../../stylesheets/usermenu.css";
+import PersonAddAltIcon from "@mui/icons-material/PersonAddAlt";
+import LockOpenIcon from "@mui/icons-material/LockOpen";
+
+function Usermenu() {
+  const [show, setShow] = useState(true);
+
+  useEffect(() => {
+    document.onclick = () => {
+      setShow((prev) => !prev);
+    };
+
+    console.log(show);
+    return () => {};
+  }, [show]);
+
+  return (
+    <div
+      className={show ? "usermenu-container" : "usermenu-container invisible"}
+    >
+      <div className="wrapper-upper">
+        <div className="usermenu-upper">
+          <div>
+            <h5>COMMUNITY</h5>
+            <ul>
+              <li>Networks</li>
+              <li>Chat</li>
+              <li>Events</li>
+              <li>Region</li>
+              <li>Directory</li>
+            </ul>
+          </div>
+          <div>
+            <h5>AAPI</h5>
+            <ul>
+              <li>Education</li>
+              <li>Benefits</li>
+              <li>Projects</li>
+              <li>Newsletters</li>
+              <li>Journals</li>
+            </ul>
+          </div>
+          <div>
+            <div className="login-container">
+              <ul>
+                <li>
+                  <PersonAddAltIcon />
+                  <span>Sign up</span>
+                </li>
+                <li>
+                  <LockOpenIcon />
+                  Log in
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="wrapper-lower">
+        <div className="usermenu-lower">
+          <div>
+            <ul>
+              <li>ABOUT</li>
+              <li>BYLAWS</li>
+              <li>CONTACT</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>CAREER</li>
+              <li>MEMBER</li>
+            </ul>
+          </div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Usermenu;
