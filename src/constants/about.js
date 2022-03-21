327924c6a3065af78a710f9953fda5e318da83b7
akshayDeq <akanojiya@deqode.com>
2022-03-21 17:28:55 +0530

    Create : Constants file for dropdown menu data

---
+++ src/constants/about.js
@@ -0,0 +1,2 @@
+export const about =
+  "Founded in 1982, the American Association of Physicians of Indian Origin represents the values and interests of more than 80,000 practicing physicians in the United States.";
