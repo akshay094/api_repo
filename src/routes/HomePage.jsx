52068cb85a5604566fb2d9ffdeea37dc05463d35
akshayDeq <akanojiya@deqode.com>
2022-03-22 13:07:34 +0530

    Create : Homepage component

---
+++ src/routes/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../components/Header/Header";
+import Usermenu from "../components/Popup_menu/Usermenu";
+import Footer from "../components/Footer/Footer";
+
+const HomePage = () => {
+  return (
+    <>
+      <Header />
+      <Usermenu />
+      <Footer />
+    </>
+  );
+};
+
+export default HomePage;
