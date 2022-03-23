4bcebf17212b017699175255f5bbe1de2551b35a
akshayDeq <akanojiya@deqode.com>
2022-03-23 18:53:57 +0530

    Update : Rename homepage component to landing page

--- src/routes/LandingPage.jsx
+++ src/routes/LandingPage.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import Header from "../components/Header/Header";
-import Usermenu from "../components/Popup_menu/Usermenu";
+import Usermenu from "../components/PopupMenu/Usermenu";
 import Footer from "../components/Footer/Footer";
+import LandingPageMid from "../components/LandingPageMid/LandingPageMid";
 
-const HomePage = () => {
+const LandingPage = () => {
   return (
     <>
       <Header />
       <Usermenu />
+      <LandingPageMid />
       <Footer />
     </>
   );
 };
 
-export default HomePage;
+export default LandingPage;
